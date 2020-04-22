ca1a38cb7a9cbf16024bae48c9a822e53f29bc88
jacando.tos <sereicikas.t@gmail.com>
2020-04-22 18:45:07 +0200

    add method on type

--- src/main.js
+++ src/main.js
@@ -2,8 +2,5 @@ import Vue from "vue";
 import App from "./App";
 
 new Vue({
-  el: "#app",
-  render: function (createElement) {
-    return createElement(App);
-  }
-});
+  render: h => h(App)
+}).$mount("#app");
