5a9f1405c82e3be772251cf417438e6ccc948b5c
jacando.tos <sereicikas.t@gmail.com>
2020-04-24 15:41:33 +0200

    render list item data

--- src/components/VideoList.vue
+++ src/components/VideoList.vue
@@ -1,15 +1,18 @@
 <template>
   <ul>
-    VideoList
-    {{
-      videos.length
-    }}
+    <VideoListItem :key="video.etag" v-for="video in videos" :video="video">
+    </VideoListItem>
   </ul>
 </template>
 
 <script>
+import VideoListItem from "./VideoListItem";
+
 export default {
   name: "VideoList",
+  components: {
+    VideoListItem
+  },
   props: ["videos"]
 };
 </script>
