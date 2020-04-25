73c3cae65aabc1e956e18bbc1e3b508dd8609505
jacando.tos <sereicikas.t@gmail.com>
2020-04-25 22:09:13 +0200

    display selected video

--- src/components/VideoDetail.vue
+++ src/components/VideoDetail.vue
@@ -1,13 +1,25 @@
 <template>
-  <div>
-    {{ video.snipped.title }}
+  <div v-if="video">
+    <div>
+      <iframe :src="videoUrl" />
+    </div>
+    <div class="details">
+      <h2>{{ video.snippet.title }}</h2>
+      <p>{{ video.snippet.description }}</p>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "videoDetail",
-  props: ["video"]
+  props: ["video"],
+  computed: {
+    videoUrl() {
+      const { videoId } = this.video.id;
+      return `https://www.youtube.com/embed/${videoId}`;
+    }
+  }
 };
 </script>
 
