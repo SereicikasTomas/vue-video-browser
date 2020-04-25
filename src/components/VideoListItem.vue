f8ee8958669588faafaff0d899c6ddc06a095629
jacando.tos <sereicikas.t@gmail.com>
2020-04-25 14:34:44 +0200

    style video list item

--- src/components/VideoListItem.vue
+++ src/components/VideoListItem.vue
@@ -1,5 +1,6 @@
 <template>
   <li>
+    <img :src="video.snippet.thumbnails.default.url" />
     {{ video.snippet.title }}
   </li>
 </template>
@@ -11,4 +12,10 @@ export default {
 };
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+li {
+  margin-top: 0.5rem;
+  padding: 1rem;
+  border: 1px solid lightblue;
+}
+</style>
